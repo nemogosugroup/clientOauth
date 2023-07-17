35de39292f1d70ae7abc556a7d455e180a67abb4
tranhuuhoang1905 <tranhuuhoang190594@gmail.com>
2023-07-17 17:10:45 +0700

    fix

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -170,7 +170,7 @@ class VoteController extends Controller
         
 
         
-        $data = Vote::select('vote.id', 'vote_questions.question as question', 'vote_questions.type as type', 'vote_questions.id as question_id', 'vote_options.option as option', 'vote_options.id as option_id','vote_options.total_voted as total_voted')
+        $data = Vote::select('vote.id', 'vote.title','vote_questions.question as question', 'vote_questions.type as type', 'vote_questions.id as question_id', 'vote_options.option as option', 'vote_options.id as option_id','vote_options.total_voted as total_voted')
         ->join('vote_questions', 'vote.id', '=', 'vote_questions.vote_id')
         ->join('vote_options', 'vote_questions.id', '=', 'vote_options.question_id')
         ->where('vote.id', $voteId)
@@ -179,6 +179,7 @@ class VoteController extends Controller
         $result = [];
         foreach ($data as $item) {
             $result[$item->id]['vote_id'] = $item->id;
+            $result[$item->id]['tiltel'] = $item->title;
             $result[$item->id]['questions'][$item->question_id]['type'] = $item->type;
             $result[$item->id]['questions'][$item->question_id][$item->question][] = [
                 'option_id'=>$item->option_id,
